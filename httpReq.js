2776b13b727ecb7f58e259a25de472b19b91ac53
Saurabh Kurve <68009826+saurabhkurve@users.noreply.github.com>
2022-12-10 21:44:14 +0530

    added http request syntax in new js file

---
+++ httpReq.js
@@ -0,0 +1,10 @@
+#HTTP Request in Javasript syntax
+
+const Http = new XMLHttpRequest();
+const url='https://jsonplaceholder.typicode.com/posts';
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
